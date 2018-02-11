b8c805411b2c241369d0acebb97de942a71499d4
Fahamida Haque <fahamida.haque@gmail.com>
2018-02-11 10:31:09 -0500

    first-6-projects

---
+++ js/portfolioJs.js
@@ -0,0 +1,15 @@
+
+
+
+$(document).ready( function() {
+
+    $('.gallery-item').hover( function() {
+       $(this).find('.img-title').stop(true, true).fadeIn(300);
+
+    }, function() {
+     $(this).find('.img-title').stop(true, true).fadeOut(100); 
+    });
+    
+ 
+
+});
